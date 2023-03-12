041beba7d2deea313663466c0614ec74d1e1f562
Viswanath <allamrajuviswanath@gmail.com>
2023-03-12 13:56:06 +0530

    routing and explore structure

--- src/components/main-component/MainComponent.jsx
+++ src/components/main-component/MainComponent.jsx
@@ -1,30 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './main.css';
 import mainBackground from '../../assets/main-background.png';
 import TextInputFilter from '../common-components/TextInputFilter/TextInputFilter';
 import ButtonComponent from '../common-components/ButtonComponent/ButtonComponent';
 import FooterComponent from '../footer-component/FooterComponent';
+import ExploreComponent from '../second-component/ExploreComponent';
 
 function MainComponent() {
+    const [currentScreen,setCurrentScreen] = useState('home');
+    useEffect(()=>{
+        switch(window.location.pathname) {
+            case '/explore':
+                setCurrentScreen('explore');
+                break;
+            case '/about':
+                setCurrentScreen('about');
+            case '/':
+                setCurrentScreen('home');
+                break;
+            default:
+                setCurrentScreen(null);
+                break;
+        }
+
+    },[window.location.pathname]);
   return (
-    <div className='mainScreen'>
-        <div className='mainScreenHeading'>
-            Rethink your living & renting
-        </div>
-        <div className='mainScreenSubHeading'>
-            Make your stay painless with us
-        </div>
-        <div className='mainScreenSearchFilters'>
-            <TextInputFilter label="City" placeholder="Select your city"/>
-            <TextInputFilter label="Dates" placeholder="Select your dates"/>
-            <TextInputFilter label="Guests" placeholder="Add guests"/>
-            <ButtonComponent />
-        </div>
+    <>
+        {currentScreen === 'home' && <div className='mainScreen'>
+            <div className='mainScreenHeading'>
+                Rethink your living & renting
+            </div>
+            <div className='mainScreenSubHeading'>
+                Make your stay painless with us
+            </div>
+            <div className='mainScreenSearchFilters'>
+                <TextInputFilter label="City" placeholder="Select your city"/>
+                <TextInputFilter label="Dates" placeholder="Select your dates"/>
+                <TextInputFilter label="Guests" placeholder="Add guests"/>
+                <ButtonComponent />
+            </div>
+        </div>}
+
+        {currentScreen === 'explore' && <div className='mainScreenHeader'>
+            <ExploreComponent />    
+        </div>}
+        
         <div className='navBars'>
             <a className='explore-navigation' href='/explore'>Explore</a>
             <a className='about-navigation' href='/about'>About Us</a>
         </div>
-    </div>
+    </>
   )
 }
 
---
+++ src/components/second-component/ExploreComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './explore.css';
+import '../main-component/main.css';
+
+function ExploreComponent() {
+  return (
+    <div className='exploreComponent'>
+      <span className='exploreTitle'>Explore</span>
+        <span className='subHeading-1'>From one-guest rooms to penthouses with pools and gardens </span>
+    </div>
+  )
+}
+
+export default ExploreComponent
