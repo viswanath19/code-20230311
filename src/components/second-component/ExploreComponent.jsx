2ca3da93b183b576bcde95a87be4a4a1dc73eb3b
Viswanath <allamrajuviswanath@gmail.com>
2023-03-22 01:07:36 +0530

    explore component subHeading updates

--- src/components/second-component/ExploreComponent.jsx
+++ src/components/second-component/ExploreComponent.jsx
@@ -3,6 +3,7 @@ import Card from '../common-components/CardViews/Card';
 import './explore.css';
 import exploreBackgroundImg1 from '../../assets/explore-1.png';
 import exploreBackgroundImg2 from '../../assets/explore-2.png';
+import ButtonComponent from '../common-components/ButtonComponent/ButtonComponent';
 
 function ExploreComponent() {
   return (
@@ -11,8 +12,16 @@ function ExploreComponent() {
         <span className='subHeading-1'>From one-guest rooms to penthouses with pools and gardens </span>
         <div className='exploreCards'>
             <Card cards={[
-                {backgroundImg:exploreBackgroundImg1,header:'Room with one king-size bed'},
-                {backgroundImg:exploreBackgroundImg2,header:'Penthouse for 8 person'}
+                {backgroundImg:exploreBackgroundImg1,header:'Room with one king-size bed',subHeader:<div>
+                  <ButtonComponent width={80} height={30} text={'35$'}/>
+                  <ButtonComponent width={80} height={30} text={'28м²'}/>
+                  <ButtonComponent width={80} height={50} text={'Book!'}/>
+                </div>},
+                {backgroundImg:exploreBackgroundImg2,header:'Penthouse for 8 person',subHeader:<div>
+                  <ButtonComponent width={80} height={30} text={'2039$'}/>
+                  <ButtonComponent width={80} height={30} text={'438м²'}/>
+                  <ButtonComponent width={80} height={50} text={'Book!'}/>
+                </div>}
             ]} />
         </div>
     </div>
